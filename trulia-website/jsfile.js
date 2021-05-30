d0745ca42785c8cd92b288900a91ebb8b38c33bc
Trish Shelton <strish123@gmail.com>
2021-05-30 15:11:01 -0400

    Responsive

---
+++ trulia-website/jsfile.js
@@ -0,0 +1,6 @@
+let modalBox = document.getElementById("modal-box");
+
+let openModal = function() {
+  modalBox.classList.toggle("open");
+  // you can create a duplicate statement here targeting another element/class if you need more
+}
