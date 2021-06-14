66698469b69926733315c8b755dad7c56e4a2d32
Trish Shelton <strish123@gmail.com>
2021-06-14 19:20:03 -0400

    Edit

--- trulia-website/jsfile.js
+++ trulia-website/jsfile.js
@@ -1,6 +1,17 @@
-let modalBox = document.getElementById("modal-box");
-
+let modalBox = document.getElementsByClassName("modal-box")[0];
 let openModal = function() {
   modalBox.classList.toggle("open");
   // you can create a duplicate statement here targeting another element/class if you need more
 }
+let closeModal = function() {
+  modalBox.classList.toggle("close");
+  // you can create a duplicate statement here targeting another element/class if you need more
+}
+
+
+
+
+
+
+
+
